56f12712d1915c4b7da5476e943ef3d3c6b09cb4
AmirScheldon <amirtaslimi12@gmail.com>
2024-04-27 00:08:06 +0330

    final project

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from . import views
+from .views import home, logout_user, login_user, register, customer_id, delete_customer, add_customer, update_customer
 
 urlpatterns = [
-    path('', views.home, name= 'home'),
+    path('', home, name= 'home'),
+    path('register/', register, name= 'register'),
+    path('login/', login_user, name= 'login'),
+    path('logout/', logout_user, name= 'logout'),
+    path('customer-id/<int:pk>', customer_id, name= 'customer-id'),
+    path('customer-id/delete/<int:pk>', delete_customer, name= 'delete-customer'),
+    path('customer-id/update/<int:pk>', update_customer, name= 'update-customer'),
+    path('add_customer', add_customer, name= 'add_customer'),    
+        
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,104 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.urls import reverse
+from .forms import SignUpForm, AddCustomerForm
+from .models import Record
+
+
+
 
 def home(request):
-    context = {}
+    record = Record.objects.all() 
+    context = {'record': record}
     return render(request, 'home.html', context)
+    
+
+def login_user(request):
+    is_login_page = request.path == reverse('login')
+    context= { 'is_login_page': is_login_page}
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # if user autheticated, it returns User object otherwise it returns None.
+        user = authenticate(request, username= username, password = password)
+        
+        if user is not None: 
+            login(request, user)
+            messages.success(request, message= 'You have been loggedin')
+            return redirect('home')
+        else:
+            messages.success(request, message= ' You are not able to login')
+            return redirect('home')
+    else :
+        return render(request, 'login.html', context)
+
+    
+        
+def logout_user(request):
+    logout(request)
+    messages.success(request, message='You have been loggedout')
+    return redirect('home')
+
+def register(request):
+    is_register_page = request.path == reverse('register')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, message= 'You have been successfully registered!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form, 'is_register_page': is_register_page})
+    
+    return render(request, 'register.html', {'form': form, 'is_register_page': is_register_page})    
+        
+
+def customer_id(request, pk):
+    if request.user.is_authenticated:
+        queryset = Record.objects.get(id=pk)
+        return render(request, 'customer-id.html', {'queryset': queryset})
+    else:
+        messages.success(request, message= 'you are not signed-up please first sign-in')
+        return redirect('home')
+    
+def delete_customer(request, pk):
+    customer = Record.objects.get(id=pk)
+    customer.delete()
+    messages.success(request, message= 'Customer successfully deleted!')
+    return redirect('home')  
+
+def add_customer(request):
+    is_customer_id_page = request.path == reverse('add_customer')
+    form = AddCustomerForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                messages.success(request, message= 'Customer successfully added!')
+                return redirect('home')             
+        return render(request, 'add-customer.html', {'form':form, 'is_customer_id_page': is_customer_id_page})
+    else:
+        messages.success(request, message= 'you are not signed-up please first sign-in')
+        return redirect('home')  
+    
+    
+def update_customer(request, pk):
+    if request.user.is_authenticated:
+        current_user = Record.objects.get(id=pk)
+        form = AddCustomerForm(request.POST or None, instance=current_user )
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                messages.success(request, message= 'Customer successfully updated!')
+                return redirect('home') 
+    else:
+        messages.success(request, message= 'you are not signed-up please first sign-in')
+        return redirect('home')                    
